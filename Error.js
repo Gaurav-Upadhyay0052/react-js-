319fd740b57c5f7dd29e311c2888be273611ff36
Gaurav-Upadhyay0052 <gu3645121@gmail.com>
2025-02-08 21:21:14 +0530

    Add initial files

--- Error.js
+++ Error.js
@@ -5,7 +5,7 @@ const Error = () => {
     return(
         <>
         <h1>Opps ! </h1>
-        <h2>something went wrong !!</h2>
+        <h2>something went wrong  and please check error !!</h2>
         <h2>{err.status + " : " + err.statusText}</h2>
         </>
     );
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # react-js-
+Author Gaurav Upadhyay
